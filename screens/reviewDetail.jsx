a501b76a27d983aaa15147dc3c5af63ec59a56a7
Andy Hu <huandy01@gmail.com>
2022-07-11 23:45:56 +0200

    Lesson 21 Navigating Around

--- screens/reviewDetail.jsx
+++ screens/reviewDetail.jsx
@@ -1,10 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { globalStyles } from "../../styles/global";
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ navigation }) {
+
+    const pressHandler = () => {
+        navigation.goBack();
+    }
+
     return (
         <View style={globalStyles.container}>
             <Text>ReviewDetails Screen</Text>
+            <Button title="Back To Home Screen" onPress={pressHandler}/>
         </View>
     )
 }
