97e2249cb3c52241ae15809b839e5cf2e4ebd129
Andy Hu <huandy01@gmail.com>
2022-07-17 23:54:22 +0200

    Lesson 22 Passing data between screens

--- screens/home.jsx
+++ screens/home.jsx
@@ -1,4 +1,5 @@
-import { View, Text, Button } from "react-native";
+import { useState } from "react";
+import { View, Text, Button, FlatList, TouchableOpacity } from "react-native";
 import { globalStyles } from "../../styles/global";
 
 export default function Home({ navigation }) {
@@ -8,9 +9,20 @@ export default function Home({ navigation }) {
         //navigation.push("ReviewDetails");
     }
 
+    const [reviews, setReviews] = useState([
+        {title: "Title 1", rating: 5, body: "Lorem Ipsum", key: "1"},
+        {title: "Title 2", rating: 3, body: "Lorem Ipsum", key: "2"},
+        {title: "Title 3", rating: 1, body: "Lorem Ipsum", key: "3"}
+    ])
+
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Home Screen</Text>
+            <FlatList data={reviews} renderItem={({ item }) => (
+                <TouchableOpacity onPress={() => navigation.navigate("ReviewDetails", {item})}>
+                    <Text style={globalStyles.titleText}>{item.title}</Text>
+                </TouchableOpacity>
+            )}/>
             <Button title="Go To Review Details" onPress={pressHandler}/>
         </View>
     )
--- screens/reviewDetail.jsx
+++ screens/reviewDetail.jsx
@@ -10,6 +10,9 @@ export default function ReviewDetails({ navigation }) {
     return (
         <View style={globalStyles.container}>
             <Text>ReviewDetails Screen</Text>
+            <Text>{navigation.getParam("title")}</Text>
+            <Text>{navigation.getParam("body")}</Text> 
+            <Text>{navigation.getParam("rating")}</Text>
             <Button title="Back To Home Screen" onPress={pressHandler}/>
         </View>
     )
