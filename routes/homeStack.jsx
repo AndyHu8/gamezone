9aba688bfac680dbd31921e466f75644f820f8e4
Andy Hu <huandy01@gmail.com>
2022-07-18 00:02:41 +0200

    Lesson 23 Navigation Options

--- routes/homeStack.jsx
+++ routes/homeStack.jsx
@@ -6,14 +6,31 @@ import ReviewDetails from "../screens/reviewDetail";
 //Reihenfolge der Screens (Oben als Erstes immer zeigen)
 const screens = {
     Home: {
-        screen: Home
+        screen: Home,
+        navigationOptions: {
+            title: "GameZone",
+            headerStyle: {
+                backgroundColor: "#333"
+            }
+        }
     },
 
     ReviewDetails: {
-        screen: ReviewDetails
+        screen: ReviewDetails,
+        navigationOptions: {
+            title: "Review Details",
+        }
     }
 }
 
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+    defaultNavigationOptions: {
+        headerTintColor: "#444",
+        headerStyle: {
+            backgroundColor: "#eee",
+            height: 60
+        }
+    }
+});
 
 export default createAppContainer(HomeStack);
