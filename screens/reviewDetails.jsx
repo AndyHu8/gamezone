d61b46879ab57a7b8bc550dace5713718bc629fd
Andy Hu <huandy01@gmail.com>
2022-09-01 22:58:30 +0200

    Lesson 27

--- screens/reviewDetails.jsx
+++ screens/reviewDetails.jsx
@@ -1,5 +1,5 @@
-import { View, Text, Button } from "react-native";
-import { globalStyles } from "../../styles/global";
+import { View, Text, Image, StyleSheet } from "react-native";
+import { globalStyles, images } from "../../styles/global";
 import Card from "../shared/card";
 
 export default function ReviewDetails({ navigation }) {
@@ -8,15 +8,31 @@ export default function ReviewDetails({ navigation }) {
         navigation.goBack();
     }
 
+    const rating = navigation.getParam("rating");
+
     return (
         <View style={globalStyles.container}>
             <Card>
                 <Text>ReviewDetails Screen</Text>
                 <Text>{navigation.getParam("title")}</Text>
                 <Text>{navigation.getParam("body")}</Text>
-                <Text>{navigation.getParam("rating")}</Text>
+                <View style={styles.ratingStyle}>
+                    <Text>GameZone rating: </Text>
+                    <Image source={require(images.ratings[rating])}/>
+                </View>
             </Card>
             <Button title="Back To Home Screen" onPress={pressHandler}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    ratingStyle: {
+        flexDirection: "row",
+        justifyContent: "center",
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: "#eee",
+    }
+});
