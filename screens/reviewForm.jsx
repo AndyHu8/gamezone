061903e1bc82a47d75747c677a526e13f29176bb
Andy Hu <huandy01@gmail.com>
2022-10-04 10:41:26 +0200

    Lesson 33

--- screens/reviewForm.jsx
+++ screens/reviewForm.jsx
@@ -26,21 +26,27 @@ export default function ReviewForm({ addReview }) {
                         style={globalStyles.input} 
                         placeholder='Review title' 
                         onChangeText={props.handleChange('title')} 
-                        value={props.values.title}/>
+                        value={props.values.title}
+                        onBlur={props.handleBlur('title')}/>
+                        <Text style={globalStyles.errorText}>{props.touched.title && props.errors.title}</Text>
                         
                         <TextInput 
                         style={globalStyles.input} 
                         multiline
                         placeholder='Review body' 
                         onChangeText={props.handleChange('body')} 
-                        value={props.values.body}/>
+                        value={props.values.body}
+                        onBlur={props.handleBlur('body')}/>
+                        <Text style={globalStyles.errorText}>{props.touched.body && props.errors.body}</Text>
 
                         <TextInput 
                         style={globalStyles.input} 
                         placeholder='Rating (1-5)' 
                         onChangeText={props.handleChange('rating')} 
                         value={props.values.rating}
+                        onBlur={props.handleBlur('rating')}
                         keyboardType='numeric'/>
+                        <Text style={globalStyles.errorText}>{props.touched.rating && props.errors.rating}</Text>
 
                         <Button title="submit" color="maroon" onPress={props.handleSubmit}></Button>
                     </View>
